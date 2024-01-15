c7bd5d4a6b5e7821e386f15391a84157e543ff5a
Sanket Koirala <koiralasanket11@gmail.com>
2024-01-14 20:11:56 -0600

    New components created for Landing page and new homework page

---
+++ src/app/new-homework/new-homework.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-homework',
+  templateUrl: './new-homework.component.html',
+  styleUrls: ['./new-homework.component.scss']
+})
+export class NewHomeworkComponent {
+
+}
